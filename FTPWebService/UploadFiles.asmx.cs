dae44193a80365690724f7449ae9e240a12f2dd7
Patrick Kasteel <kplkasteel@hotmail.com>
2018-08-13 14:07:18 +0200

    Minor Changes

--- FTPWebService/UploadFiles.asmx.cs
+++ FTPWebService/UploadFiles.asmx.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Web;
 using System.Web.Services;
 
 namespace FTPWebService
@@ -17,82 +14,96 @@ namespace FTPWebService
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
-    public class UploadFiles : System.Web.Services.WebService
+    public class UploadFiles :WebService
     {
-
+        private string _message;
         [WebMethod]
-        public string SaveAndUpload(string ftpSite, string ftpUsername, string ftpPassword, string reportLocation)
+        public string SaveAndUpload()
         {
-            var ftp = new Ftp();
-            return ftp.UploadFileToServer(new Uri(ftpSite), ftpUsername, ftpPassword, reportLocation);
+            UploadFileToServer("ftp://cloudplatform.iconnix.co.za", "Mobile1", "Mobile1",@"C:\NashuaReports");
+
+            DeleteFiles(@"C:\NashuaReports");
+            return _message;
         }
 
-        public class Ftp
-        {
-            public string UploadFileToServer(Uri serverUri, string username, string password, string fileName)
+        public void UploadFileToServer(string url, string username, string password, string fileName)
+        {   
+            var serverUri = new Uri(url);
+            if (serverUri.Scheme != Uri.UriSchemeFtp)
             {
-                var message = string.Empty;
-                if (serverUri.Scheme != Uri.UriSchemeFtp) return "Incorrect FTP URL";
+                _message = "Incorrect FTP URL";
+                return;
+            }
+              
 
-                try
+            try
+            {
+                var fileArray = Directory.GetFiles(fileName, "*.xl*");
+                if (!fileArray.Any())
                 {
-                    var fileArray = Directory.GetFiles(fileName, "*.xl*");
-                    if (!fileArray.Any()) return "No Attachments found";
-
-                    foreach (var file in fileArray)
-                    {
-                        // Get the object used to communicate with the server.
-                        var request = (FtpWebRequest) WebRequest.Create(serverUri + @"/" + Path.GetFileName(file));
-                        request.Credentials
-                            = new NetworkCredential(username, password);
-                        request.Method = WebRequestMethods.Ftp.UploadFile;
-                        // Don't set a time limit for the operation to complete.
-                        request.Timeout = System.Threading.Timeout.Infinite;
-
-                        // Copy the file contents to the request stream.
-                        const int bufferLength = 2048;
-                        var buffer = new byte[bufferLength];
-
-                        var count = 0;
-                        int readBytes;
-                        var stream = File.OpenRead(file);
-                        var requestStream = request.GetRequestStream();
-                        do
-                        {
-                            readBytes = stream.Read(buffer, 0, bufferLength);
-                            requestStream.Write(buffer, 0, bufferLength);
-                            count += readBytes;
-                        } while (readBytes != 0);
-
-                        //Console.WriteLine("Writing {0} bytes to the stream.", count);
-                        // IMPORTANT: Close the request stream before sending the request.
-                        requestStream.Close();
-
-                        var response = (FtpWebResponse) request.GetResponse();
-                        if (response.StatusCode.ToString() == "226")
-                        {
-                            message = "226";
-                        }
-                        message = $"Upload status: {response.StatusCode} {response.StatusDescription}";
-                        response.Close();
-                    }
-
-                    var deleteFileArray = Directory.GetFiles(fileName);
-                    if (deleteFileArray.Length > 0)
-                    {
-                        foreach (var file in deleteFileArray)
-                        {
-                            File.Delete(file);
-                        }
-                    }
-
+                   _message = "No Attachments found";
+                    return;
                 }
-                catch (Exception e)
+
+                foreach (var file in fileArray)
                 {
-                    message = e.InnerException?.ToString();
+                    // Get the object used to communicate with the server.
+                    var request = (FtpWebRequest)WebRequest.Create(serverUri + @"/" + Path.GetFileName(file));
+                    request.Credentials
+                        = new NetworkCredential(username, password);
+                    request.Method = WebRequestMethods.Ftp.UploadFile;
+                    // Don't set a time limit for the operation to complete.
+                    request.Timeout = System.Threading.Timeout.Infinite;
+
+                    // Copy the file contents to the request stream.
+                    const int bufferLength = 2048;
+                    var buffer = new byte[bufferLength];
+
+                    var count = 0;
+                    int readBytes;
+                    var stream = File.OpenRead(file);
+                    var requestStream = request.GetRequestStream();
+                    do
+                    {
+                        readBytes = stream.Read(buffer, 0, bufferLength);
+                        requestStream.Write(buffer, 0, bufferLength);
+                        count += readBytes;
+                    } while (readBytes != 0);
+                    Console.WriteLine("Writing {0} bytes to the stream.", count);
+                    // IMPORTANT: Close the request stream before sending the request.
+                    requestStream.Close();
+
+                    var response = (FtpWebResponse)request.GetResponse();
+                    var code = response.StatusCode;
+                    if (response.StatusCode == FtpStatusCode.ClosingData)
+                    {
+                        _message = "226";
+                    }
+                    else
+                    {
+                        _message = $"Upload status: {response.StatusCode} {response.StatusDescription}";
+                    }
+                    response.Close();
+                    stream.Close();
                 }
-                return message;
+            }
+            catch (Exception e)
+            {
+                _message = e.ToString();
+            }
+
+        }
+
+        private static void DeleteFiles(string filePatch)
+        {
+            var deleteFileArray = Directory.GetFiles(filePatch);
+            if (deleteFileArray.Length <= 0) return;
+            foreach (var file in deleteFileArray)
+            {
+                var fileInfo = new FileInfo(file);
+                fileInfo.Delete();
             }
         }
     }
+
 }
